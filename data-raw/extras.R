dcabedb6fa88c8c7cb0a3176d42f0c0a8d568430
AllanJ93 <al.josue93@gmail.com>
2024-06-02 18:18:40 -0700

    Corte 1615

--- data-raw/extras.R
+++ data-raw/extras.R
@@ -30,3 +30,22 @@ muestra_shp |>
   distinct(id, municip) |>
   mutate(municip = gsub(pattern = "[0-9]. ", replacement = "", x = municip)) |>
   left_join(bd_equipos, by = c("municip" = "NOMBRE"))
+
+
+bd_encuesta_salida |>
+  select(id, voto_sen_candidato) |>
+  mutate(voto_sen_candidato = ifelse(grepl('Gustavo Sánchez y Guadalupe Gutiérrez', voto_sen_candidato),
+                                     'Gustavo Sánchez y Guadalupe Gutiérrez del PAN PRI PRD',voto_sen_candidato)) |>
+  group_by(id) |>
+  count(voto_sen_candidato) |>
+  ungroup() |>
+  tidyr::pivot_wider(id_cols = id, names_from = voto_sen_candidato, values_from = n) |>
+  mutate(across(.cols =  !id, .fns = ~ tidyr::replace_na(data = ., replace = 0))) |>
+  arrange(desc(`Gustavo Sánchez y Guadalupe Gutiérrez del PAN PRI PRD`)) |>
+  select(id, `Gustavo Sánchez y Guadalupe Gutiérrez del PAN PRI PRD`) |> print(n = Inf)
+  summary(n)
+  # filter(grepl(pattern = "0398", x = id)) |>
+  print(n = Inf)
+  head(10) |>
+  select(id)
+
